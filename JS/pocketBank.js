7d03a08b6a8b15f5325ca4423b3ce57601f3ec8d
shahriarshadhin <shadhin19shahriar@gmail.com>
2022-02-17 20:08:28 +0600

    income and balance done

--- JS/pocketBank.js
+++ JS/pocketBank.js
@@ -1,3 +1,5 @@
+let incomeAmount;
+
 function expense(inputId) {
 
     const ExpenseInput = document.getElementById(inputId);
@@ -11,6 +13,9 @@ function expense(inputId) {
 
 document.getElementById('calculate-btn').addEventListener('click', function() {
 
+
+    let incomeAmount = expense('income');
+
     const foodExpenseAmount = expense('food');
 
     const rentExpenseAmount = expense('rent');
@@ -20,12 +25,75 @@ document.getElementById('calculate-btn').addEventListener('click', function() {
 
     // get total expense
 
+    const mianIncome = document.getElementById('main-income');
+    const incomeText = mianIncome.innerText;
+    const amountIncome = parseFloat(incomeText);
+    mianIncome.innerText = incomeAmount;
+
+
+
     const totalExpense = document.getElementById('total-expenses');
     const totalExpenseText = totalExpense.innerText;
     const totalExpenseAmount = parseFloat(totalExpenseText);
+
     totalExpense.innerText = foodExpenseAmount + rentExpenseAmount + clothesExpenseAmount;
 
 
+
+
+    const balance = document.getElementById('balance-field');
+    const balanceText = balance.innerText;
+    const balanceAmount = parseFloat(balanceText);
+
+    if (totalExpense.innerText > incomeAmount) {
+        alert('You are running out of money');
+    } else {
+        balance.innerText = incomeAmount - totalExpense.innerText;
+    }
+
+    // const savingInput = document.getElementById('saving');
+    // const savingAmountText = savingInput.value;
+    // const savingAmount = parseFloat(savingAmountText);
+    // savingInput.innerText = incomeAmount * 100;
+
+
+
+
+
+
+});
+
+
+function savingCalculation() {
+
+    const savingInput = document.getElementById('saving');
+    const savingAmountText = savingInput.value;
+    const savingAmount = parseFloat(savingAmountText);
+
+    savingInput.innerText = incomeAmount * savingInput / 100;
+
+
+
+}
+
+
+
+document.getElementById('save-btn').addEventListener('click', function() {
+
+    // const incomeInput = document.getElementById('income');
+    savingCalculation();
+    // const incomeAmountText = incomeInput.value;
+    // const incomeAmount = parseFloat(incomeAmountText);
+
+
+    // const savingInput = document.getElementById('saving');
+    // const savingAmountText = savingInput.value;
+    // const savingAmount = parseFloat(savingAmountText);
+
+    // savingInput.innerText = incomeAmount * savingInput / 100;
+
+    // // document.getElementById("saving").innerHTML = savedAmount;
+    // // document.getElementById("rest-balance").innerHTML = afterSave;
 
 
 })
